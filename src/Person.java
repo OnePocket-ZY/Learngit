9cb4d66b7903bc1269b7b0e4cd5c45a37080903b
ZhaoYang <zhaoyang.fox@gmail.com>
2018-01-16 10:48:08 +0800

    去除有整型实参的函数

--- src/Person.java
+++ src/Person.java
@@ -14,9 +14,9 @@ class Reload_A{
 	//	System.out.println("这是一个无型参的函数；");
 	//}
 	
-	void funReload_A(int i){
-		System.out.println("这是一个整型实参的函数；");
-	}
+	//void funReload_A(int i){
+	//	System.out.println("这是一个整型实参的函数；");
+	//}
 	
 	void funReload_A(int i, double d){
 		System.out.println("这是有一个整型实参和一个双精度实参的函数。");
